ffbcf5dc009065982338bb3a8c0a69f548fe8433
Aiden <kim_aiden@naver.com>
2023-07-10 17:17:52 +0900

    메시지, 국제화 스프링 메시지 소스 설정 스프링 메시지 소스 사용 웹 애플리케이션에 메시지 적용하기 웹 애플리케이션에 국제화 적용하기

---
+++ message/src/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package hello.itemservice;
+
+import org.assertj.core.api.AbstractThrowableAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
