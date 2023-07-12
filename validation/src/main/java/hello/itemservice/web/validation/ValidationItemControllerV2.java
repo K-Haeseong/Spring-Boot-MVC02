642c3cb68e496f31333c3c75f8c8b3baf57a2925
Aiden <kim_aiden@naver.com>
2023-07-13 00:33:07 +0900

    오류 코드와 메시지 처리

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -61,7 +61,7 @@ public class ValidationItemControllerV2 {
             bindingResult.addError(new FieldError("item", "price", "가격은 1천원 ~ 100만원까지 허용합니다."));
         }
 
-        if(item.getQuantity() == null || item.getQuantity() > 9999) {
+        if(item.getQuantity() == null || item.getQuantity() > 10000) {
             bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9999개 까지 허용합니다."));
         }
 
@@ -93,7 +93,7 @@ public class ValidationItemControllerV2 {
     }
 
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item
             ,BindingResult bindingResult
             ,RedirectAttributes redirectAttributes) {
@@ -108,7 +108,7 @@ public class ValidationItemControllerV2 {
             bindingResult.addError(new FieldError("item", "price", item.getPrice(), false,  null, null, "가격은 1천원 ~ 100만원까지 허용합니다."));
         }
 
-        if(item.getQuantity() == null || item.getQuantity() > 9999) {
+        if(item.getQuantity() == null || item.getQuantity() > 10000) {
             bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9999개 까지 허용합니다."));
         }
 
@@ -121,6 +121,110 @@ public class ValidationItemControllerV2 {
         }
 
         // 검증실패 : 에러가 1개라도 발생 했을 경우
+        if(bindingResult.hasErrors()) {
+            log.info("errors ={}", bindingResult);
+
+            /*
+            Field error in object 'item' on field 'itemName': rejected value [null]; codes []; arguments []; default message [상품명은 필수입니다.]
+            Field error in object 'item' on field 'price': rejected value [null]; codes []; arguments []; default message [가격은 1천원 ~ 100만원까지 허용합니다.]
+            Field error in object 'item' on field 'quantity': rejected value [null]; codes []; arguments []; default message [수량은 최대 9999개 까지 허용합니다.]
+            */
+
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item
+            ,BindingResult bindingResult
+            ,RedirectAttributes redirectAttributes) {
+
+        // 검증 로직
+//      if(item.getItemName().equals("")) {
+        if(!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[] {"required.item.itemName"}, null, null));
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[] {"range.item.price"}, new Object[] {"1천원","1백만원"}, null));
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() > 10000) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[] {"max.item.quantity"}, new Object[]{9999}, null));
+        }
+
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+//              bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값은 = " + resultPrice));
+                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{"10,000원",resultPrice}, null));
+            }
+        }
+
+        // 검증실패 : 에러가 1개라도 발생 했을 경우
+        if(bindingResult.hasErrors()) {
+            log.info("errors ={}", bindingResult);
+
+            /*
+            Field error in object 'item' on field 'itemName': rejected value [null]; codes []; arguments []; default message [상품명은 필수입니다.]
+            Field error in object 'item' on field 'price': rejected value [null]; codes []; arguments []; default message [가격은 1천원 ~ 100만원까지 허용합니다.]
+            Field error in object 'item' on field 'quantity': rejected value [null]; codes []; arguments []; default message [수량은 최대 9999개 까지 허용합니다.]
+            */
+
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+    @PostMapping("/add")
+    public String addItemV4(
+            @ModelAttribute Item item
+            ,BindingResult bindingResult
+            ,RedirectAttributes redirectAttributes) {
+
+        // 바인딩 실패
+        if(bindingResult.hasErrors()) {
+            log.info("errors ={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 검증 로직
+//      if(item.getItemName().equals("")) {
+        if(!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName","required");
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price","range", new Object[] {1000, 1000000}, null);
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() > 10000) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+//              bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값은 = " + resultPrice));
+//              bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{"10,000원",resultPrice}, null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증실패 : 에러가 1개라도 있어? 그러면 실패야.
         if(bindingResult.hasErrors()) {
             log.info("errors ={}", bindingResult);
 
