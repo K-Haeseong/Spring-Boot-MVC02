54e600984d09b25507bcea4679163ae6b0f92fff
Aiden <kim_aiden@naver.com>
2023-07-24 18:47:18 +0900

    API 예외처리 HandlerExceptionResolver 02 스프링이 제공하는 ExceptionResolver

--- exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
+++ exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -14,15 +14,16 @@ public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
         try {
-            if (ex instanceof IllegalArgumentException) {
-                log.info("IllegalArgumentException resolver to 400");
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST,
-                        ex.getMessage());
+            if(ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentException resolve to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
                 return new ModelAndView();
             }
+
         } catch (IOException e) {
-            log.error("resolver ex", e);
+            log.info("resolver ex ", e);
         }
+
         return null;
     }
 }
