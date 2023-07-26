eed1128ee9d393dfb2bc090e8f13967582b48ddf
Aiden <kim_aiden@naver.com>
2023-07-27 00:48:12 +0900

    스프링 타입 컨버터02 Formatter 포맷터를 지원하는 컨버전 서비스 - FormattingConversionService Formatter 적용 스프링 제공 기본 Formatter

---
+++ typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,40 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FormattingConversionServiceTest {
+
+    DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
+    @Test
+    void formattingConversionService() {
+        // 1. 컨버터 등록
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // 2. 포맷터 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        // 1-1 컨버터 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        String string = conversionService.convert(new IpPort("127.0.0.1", 8888), String.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+        assertThat(string).isEqualTo("127.0.0.1:8888");
+
+
+        // 2-1 포맷터 사용
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+        assertThat(conversionService.convert(1000L, String.class)).isEqualTo("1,000");
+
+
+
+    }
+
+}
